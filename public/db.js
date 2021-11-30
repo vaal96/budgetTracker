f3018eb111c9b90cf702dfb274245f25336a5129
Valeria Aguilar <valeriaaguilar@live.com>
2021-11-30 12:51:56 -0800

    final updates/testing

--- public/db.js
+++ public/db.js
@@ -9,12 +9,13 @@ let db;
 const request = indexedDB.open("budget", 1);
 
 request.onupgradeneeded = ({ target }) => {
-  let db = target.result;
-  db.createObjectStore("pending", { autoIncrement: true });
+  target.result.createObjectStore("pending", { autoIncrement: true });
 };
 
 request.onsuccess = ({ target }) => {
   db = target.result;
+
+  // check if app is online before reading from db
   if (navigator.onLine) {
     checkDatabase();
   }
@@ -25,59 +26,44 @@ request.onerror = function(event) {
 };
 
 function saveRecord(record) {
-  console.log("Save record invoked");
-  // Create a transaction on the BudgetStore db with readwrite access
-  const transaction = db.transaction(["pending"], "readwrite");
-  // Access your BudgetStore object store
-  const store = transaction.objectStore("pending");
-  // Add record to your store with add method.
-  store.add(record);
+  
+  const transaction = db.transaction(['pending'], 'readwrite');
+  const pending = transaction.objectStore('pending');
+
+  console.log(record);
+
+  pending.add(record);
 }
 
 function checkDatabase() {
-  console.log("check db invoked");
-  // Open a transaction on your BudgetStore db
-  const transaction = db.transaction(["BudgetStore"], "readonly");
-  // access your BudgetStore object
-  const store = transaction.objectStore("BudgetStore");
+  
+  const transaction = db.transaction(['pending'], 'readwrite');
+  const pending = transaction.objectStore('pending');
 
-  // Get all records from store and set to a variable
-  const getAll = store.getAll();
-
-  // If the request was successful
-  function checkDatabase() {
-    // open a transaction on your pending db
-    const transaction = db.transaction("pending", "readwrite");
-
-    // access your pending object store
-    const store = transaction.objectStore("pending");
-    // get all records from store and set to a variable
-    const getAll = store.getAll();
-
-    getAll.onsuccess = function () {
-        if (getAll.result.length > 0) {
-            fetch('/api/transaction/bulk', {
-                method: 'POST',
-                body: JSON.stringify(getAll.result),
-                headers: {
-                    Accept: 'application/json, text/plain, */*',
-                    'Content-Type': 'application/json',
-                },
-            })
-                .then((response) => response.json())
-                .then(() => {
-                    // if successful, open a transaction on your pending db
-                    const transaction = db.transaction("pending", "readwrite");
-
-                    // access your pending object store
-                    const store = transaction.objectStore("pending");
-
-                    // clear all items in your store
-                    store.clear();
-                });
-        }
-    };
+  const getAll = pending.getAll();
+  getAll.onsuccess = async () => {
+    if (getAll.result.length === 0) {
+     
+      return;
+    }
+    
+    const response = await fetch("/api/transaction/bulk", {
+      method: "POST",
+      body: JSON.stringify(getAll.result),
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+    });
+    
+    const dbTransactions = await response.json();
+    if (dbTransactions.length > 0) {
+      const transaction = db.transaction(['pending'], 'readwrite');
+      const pending = transaction.objectStore('pending');
+      
+      pending.clear();
+    }
+  };
 }
 
-// listen for app coming back online
 window.addEventListener('online', checkDatabase);
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,65 +1,69 @@
 const FILES_TO_CACHE = [
-    "/",
-    "/index.html",
-    "/index.js",
-    "/styles.css",
-    "/db.js",
-    "/manifest.json",
-    "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png",
+  "./index.html",
+  "./styles.css",
+  "./index.js",
+  "./db.js",
+  "./manifest.json",
+  "./icons/icon-512x512.png",
+  "./icons/icon-192x192.png",
+  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
 ];
 
-const CACHE_NAME = "static-cache-v2";
+const CACHE_NAME = "static-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v1";
 
-// install
-self.addEventListener("install", function (event) {
-  // Perform install steps
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log("Opened cache");
+    caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(FILES_TO_CACHE);
     })
   );
+
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", function (event) {
-  // cache all get requests to /api routes
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function(event) {
   if (event.request.url.includes("/api/")) {
     event.respondWith(
-      caches
-        .open(DATA_CACHE_NAME)
-        .then((cache) => {
-          return fetch(event.request)
-            .then((response) => {
-              // If the response was good, clone it and store it in the cache.
-              if (response.status === 200) {
-                cache.put(event.request.url, response.clone());
-              }
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(event.request)
+          .then(response => {
+            if (response.status === 200) {
+              cache.put(event.request.url, response.clone());
+            }
 
-              return response;
-            })
-            .catch((err) => {
-              // Network request failed, try to get it from the cache.
-              return cache.match(event.request);
-            });
-        })
-        .catch((err) => console.log(err))
+            return response;
+          })
+          .catch(err => {
+            return cache.match(event.request);
+          });
+      }).catch(err => console.log(err))
     );
 
     return;
   }
 
   event.respondWith(
-    fetch(event.request).catch(function () {
-      return caches.match(event.request).then(function (response) {
-        if (response) {
-          return response;
-        } else if (event.request.headers.get("accept").includes("text/html")) {
-          // return the cached home page for all requests for html pages
-          return caches.match("/");
-        }
-      });
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
     })
   );
 });
