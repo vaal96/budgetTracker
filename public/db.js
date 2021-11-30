19de8301fccecee0dfb202910a4238b8ed3cbe1e
Valeria Aguilar <valeriaaguilar@live.com>
2021-11-30 11:48:44 -0800

    initial setup

--- public/db.js
+++ public/db.js
@@ -9,13 +9,12 @@ let db;
 const request = indexedDB.open("budget", 1);
 
 request.onupgradeneeded = ({ target }) => {
-  target.result.createObjectStore("pending", { autoIncrement: true });
+  let db = target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
 };
 
 request.onsuccess = ({ target }) => {
   db = target.result;
-
-  // check if app is online before reading from db
   if (navigator.onLine) {
     checkDatabase();
   }
@@ -28,20 +27,17 @@ request.onerror = function(event) {
 function saveRecord(record) {
   console.log("Save record invoked");
   // Create a transaction on the BudgetStore db with readwrite access
-  const transaction = db.transaction(["BudgetStore"], "readwrite");
-
+  const transaction = db.transaction(["pending"], "readwrite");
   // Access your BudgetStore object store
-  const store = transaction.objectStore("BudgetStore");
-
+  const store = transaction.objectStore("pending");
   // Add record to your store with add method.
   store.add(record);
+}
 
 function checkDatabase() {
   console.log("check db invoked");
-
   // Open a transaction on your BudgetStore db
   const transaction = db.transaction(["BudgetStore"], "readonly");
-
   // access your BudgetStore object
   const store = transaction.objectStore("BudgetStore");
 
@@ -49,34 +45,38 @@ function checkDatabase() {
   const getAll = store.getAll();
 
   // If the request was successful
-  getAll.onsuccess = async function () {
-    if (getAll.result.length === 0) {
-      // no items to post to backend
-      return;
-    }
-    // If there are items in the store, we need to bulk add them when we are back online
-    const response = await fetch("/api/transaction/bulk", {
-      method: "POST",
-      body: JSON.stringify(getAll.result),
-      headers: {
-        Accept: "application/json, text/plain, */*",
-        "Content-Type": "application/json",
-      },
-    });
-    const dbTransactions = await response.json();
-    // If our returned response is not empty
-    if (dbTransactions.length > 0) {
-      // Open another transaction to BudgetStore with the ability to read and write
-      const delTxn = db.transaction(["BudgetStore"], "readwrite");
+  function checkDatabase() {
+    // open a transaction on your pending db
+    const transaction = db.transaction("pending", "readwrite");
 
-      // Assign the current store to a variable
-      const currentStore = delTxn.objectStore("BudgetStore");
+    // access your pending object store
+    const store = transaction.objectStore("pending");
+    // get all records from store and set to a variable
+    const getAll = store.getAll();
 
-      // Clear existing entries because our bulk add was successful
-      currentStore.clear();
-      console.log("Clearing store 🧹");
-    }
-  };}
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json',
+                },
+            })
+                .then((response) => response.json())
+                .then(() => {
+                    // if successful, open a transaction on your pending db
+                    const transaction = db.transaction("pending", "readwrite");
+
+                    // access your pending object store
+                    const store = transaction.objectStore("pending");
+
+                    // clear all items in your store
+                    store.clear();
+                });
+        }
+    };
 }
 
 // listen for app coming back online
---
+++ public/service-worker.js
@@ -0,0 +1,65 @@
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/index.js",
+    "/styles.css",
+    "/db.js",
+    "/manifest.json",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+// install
+self.addEventListener("install", function (event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("Opened cache");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  // cache all get requests to /api routes
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(event.request)
+            .then((response) => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(event.request.url, response.clone());
+              }
+
+              return response;
+            })
+            .catch((err) => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(event.request);
+            });
+        })
+        .catch((err) => console.log(err))
+    );
+
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request).catch(function () {
+      return caches.match(event.request).then(function (response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get("accept").includes("text/html")) {
+          // return the cached home page for all requests for html pages
+          return caches.match("/");
+        }
+      });
+    })
+  );
+});
